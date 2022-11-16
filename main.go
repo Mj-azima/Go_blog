6235c4dc98f34b7a4bd3f777d3b2e81b95226969
The Mj <m.javad1391@gmail.com>
2022-11-16 16:50:36 +0330

    install html template engine and config it

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"blog/src/database"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
 )
 
 func main() {
-
-	app := fiber.New(fiber.Config{})
+	engine := html.New("./src/views/templates", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 
 	database.ConnectDb()
 
